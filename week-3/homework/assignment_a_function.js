92d97331c471c55464ecbe2c90055b944612af5e
Champ Ramentio <champramentio@gmail.com>
2023-09-21 18:40:04 +0700

    Added function version of assignment A

---
+++ week-3/homework/assignment_a_function.js
@@ -0,0 +1,43 @@
+function decrypting(encrypted_message, hints) {
+	let decrypted_message = "";
+
+	for (let i = 0; i < encrypted_message.length; i++) {
+		let character = encrypted_message[i];
+
+		for (const hint of hints) {
+			if (encrypted_message.slice(i, i + hint.from.length) === hint.from) {
+				character = hint.to;
+				i += hint.from.length - 1; //skip karakter yang sudah diganti
+				break;
+			}
+		}
+
+		decrypted_message += character;
+	}
+
+	return decrypted_message;
+}
+
+const encrypted_message = "M@** 5t &td1!gqt und@x tq@ o$* tx@@ #* tq@ %3d l4gqtqous@";
+
+const hint = [
+	{ from: "@", to: "e" },
+	{ from: "4", to: "i" },
+	{ from: "x", to: "r" },
+	{ from: "z", to: "k" },
+	{ from: "q", to: "h" },
+	{ from: "5", to: "a" }
+];
+
+const hint2 = [
+	{ from: "**", to: "et" },
+	{ from: "&t", to: "mi" },
+	{ from: "$*", to: "ak" },
+	{ from: "#*", to: "by" },
+	{ from: "%3", to: "ol" },
+	{ from: "1!", to: "ni" }
+];
+
+const message = decrypting(encrypted_message, [...hint, ...hint2]);
+
+console.log(message);
