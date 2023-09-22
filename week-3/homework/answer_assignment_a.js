cba4d1eb69a2a6b1f4c42ca488744c0c7bfd3867
Champ Ramentio <champramentio@gmail.com>
2023-09-22 16:21:51 +0700

    Splitted assignment A into 2 parts

--- week-3/homework/answer_assignment_a.js
+++ week-3/homework/answer_assignment_a.js
@@ -1,45 +1,45 @@
 function decrypting(encrypted_message, hints) {
-  let decrypted_message = "";
+	let decrypted_message = "";
 
-  for (let i = 0; i < encrypted_message.length; i++) {
-    let character = encrypted_message[i];
+	for (let i = 0; i < encrypted_message.length; i++) {
+		let character = encrypted_message[i];
 
-    for (const hint of hints) {
-      if (encrypted_message.slice(i, i + hint.from.length) === hint.from) {
-        character = hint.to;
-        i += hint.from.length - 1; //skip karakter yang sudah diganti
-        break;
-      }
-    }
+		for (const hint of hints) {
+			if (encrypted_message.slice(i, i + hint.from.length) === hint.from) {
+				character = hint.to;
+				i += hint.from.length - 1; //skip karakter yang sudah diganti
+				break;
+			}
+		}
 
-    decrypted_message += character;
-  }
+		decrypted_message += character;
+	}
 
-  return decrypted_message;
+	return decrypted_message;
 }
 
-const encrypted_message =
-  "M@** 5t &td1!gqt und@x tq@ o$* tx@@ #* tq@ %3d l4gqtqous@";
+const encrypted_message = "M@** 5t &td1!gqt und@x tq@ o$* tx@@ #* tq@ %3d l4gqtqous@";
 
 const hint = [
-  { from: "@", to: "e" },
-  { from: "4", to: "i" },
-  { from: "x", to: "r" },
-  { from: "z", to: "k" },
-  { from: "q", to: "h" },
-  { from: "5", to: "a" },
+	{ from: "@", to: "e" },
+	{ from: "4", to: "i" },
+	{ from: "x", to: "r" },
+	{ from: "z", to: "k" },
+	{ from: "q", to: "h" },
+	{ from: "5", to: "a" }
 ];
 
 const hint2 = [
-  { from: "**", to: "et" },
-  { from: "&t", to: "mi" },
-  { from: "$*", to: "ak" },
-  { from: "#*", to: "by" },
-  { from: "%3", to: "ol" },
-  { from: "1!", to: "ni" },
+	{ from: "**", to: "et" },
+	{ from: "&t", to: "mi" },
+	{ from: "$*", to: "ak" },
+	{ from: "#*", to: "by" },
+	{ from: "%3", to: "ol" },
+	{ from: "1!", to: "ni" }
 ];
 
 const message1 = decrypting(encrypted_message, hint);
-console.log(message1);
+console.log("message 1: ", message1);
+
 const message2 = decrypting(message1, hint2);
-console.log(message2);
+console.log("message 2: ", message2);
