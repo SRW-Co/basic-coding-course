681e236b46225a7535dab87088fd0e5e4ef42f1b
Champ Ramentio <champramentio@gmail.com>
2023-09-29 08:57:08 +0700

    Added exercise for W5

---
+++ week-5/exercise.js
@@ -0,0 +1,38 @@
+const klien1 = {
+	nama: "Danareksa",
+	tipe: "BUMN",
+	lead: "Callista",
+	member: ["Fajar", "Kevin", "Rizky", "Syiffa"],
+	total: 300_000_000,
+	durasi: 6
+};
+
+const klien2 = {
+	nama: "PPA",
+	tipe: "BUMN",
+	lead: "Jufina",
+	member: ["Ali", "Einstain", "Alma", "Icha"],
+	total: 150_000_000,
+	durasi: 4
+};
+
+const klien3 = {
+	nama: "BJB",
+	tipe: "NON_BUMN",
+	lead: "Aryo",
+	member: ["Kevin", "Mei", "Angga"],
+	total: 200_000_000,
+	durasi: 3
+};
+
+const daftar_klien = [klien1, klien2, klien3];
+
+//1. tampilkan semua project yang di lead oleh Callista
+
+//2. tampilkan semua project yang bertipe BUMN
+
+//3. tampikan informasi skala project: jika member diatas 3 orang berskala besar, jika tidak maka berskala kecil
+
+//4. tampilkan semua project yang berskala kecil
+
+//5. hitung total nominal semua project SRW&Co.
