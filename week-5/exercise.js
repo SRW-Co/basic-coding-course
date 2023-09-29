80018f1965773f883d414df1b5404ebf96d9b227
Champ Ramentio <champramentio@gmail.com>
2023-09-29 09:06:45 +0700

    Added answer on last commit

--- week-5/exercise.js
+++ week-5/exercise.js
@@ -28,11 +28,38 @@ const klien3 = {
 const daftar_klien = [klien1, klien2, klien3];
 
 //1. tampilkan semua project yang di lead oleh Callista
+const hasil1 = daftar_klien.filter(x => x.lead === "Callista");
+console.log(" hasil1 ", hasil1);
 
 //2. tampilkan semua project yang bertipe BUMN
+const hasil2 = daftar_klien.filter(x => x.tipe === "BUMN");
+console.log(" hasil2 ", hasil2);
 
 //3. tampikan informasi skala project: jika member diatas 3 orang berskala besar, jika tidak maka berskala kecil
+const hasil3 = daftar_klien.map(x => {
+	const jumlah_anggota = x.member.length;
+
+	let skala;
+
+	if (jumlah_anggota > 3) skala = "Besar";
+	else skala = "Kecil";
+
+	return {
+		...x,
+		skala: skala
+	};
+});
+console.log(" hasil3 ", hasil3);
 
 //4. tampilkan semua project yang berskala kecil
+const hasil4 = hasil3.filter(x => x.skala === "Kecil");
+console.log(" hasil4 ", hasil4);
 
 //5. hitung total nominal semua project SRW&Co.
+let total_proyek = 0;
+
+for (let i = 0; i < daftar_klien.length; i++) {
+	total_proyek = total_proyek + daftar_klien[i].total;
+}
+
+console.log(" total proyek ", total_proyek);
